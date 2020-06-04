b05136130211d18ec1597b58b513cd170e6ba2fe
Pablo <pabarragan94@gmail.com>
2020-06-04 18:30:06 +0200

    Initial

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # blur-overlay-rr
 Small userscript that add blur effect to RivalRegions game overlay in PC version
+Requires Tampermonkey
---
+++ blur-overlay-rr.js
@@ -0,0 +1,52 @@
+// ==UserScript==
+// @name         BlurOverlayRR
+// @namespace    https://github.com/pbl0
+// @version      0.1
+// @description  Small userscript that adds blur effect to RivalRegions game overlay in PC version
+// @author       Pablo
+// @match        https://rivalregions.com/*
+// @grant        none
+// ==/UserScript==
+
+const BLUR = "10";
+
+(function() {
+    'use strict';
+
+
+    // select the target node
+    var target = document.querySelector('#header_slide');
+
+    // create an observer instance
+    var observer = new MutationObserver(function(mutations) {
+        mutations.forEach(function(mutation) {
+            var filterVal;
+            if ($("#header_slide").height() <= 0){
+                filterVal = 'blur(0px)';
+            } else{
+                filterVal = 'blur('+ BLUR + 'px)';
+            }
+
+            $("#content")
+                .css('filter',filterVal)
+                .css('webkitFilter',filterVal)
+                .css('mozFilter',filterVal)
+                .css('oFilter',filterVal)
+                .css('msFilter',filterVal);
+            $("#header_menu")
+                .css('filter',filterVal)
+                .css('webkitFilter',filterVal)
+                .css('mozFilter',filterVal)
+                .css('oFilter',filterVal)
+                .css('msFilter',filterVal);
+        });
+    });
+
+    // configuration of the observer:
+    var config = { attributes: true, childList: true, characterData: true };
+
+    // pass in the target node, as well as the observer options
+    observer.observe(target, config);
+
+
+})();
